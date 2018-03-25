c1467b1abd1d8c21e6f1bb376b190d82d9627fd7
azujuuuuuun <azujuuuuuun@gmail.com>
2018-03-26 01:08:02 +0900

    [update]送信ボタン押下時の処理をajaxに変更

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
-let queryParameter = location.search.substring(1);
-let accessToken = document.getElementById('access-token');
-accessToken.value = queryParameter;
+document.addEventListener('DOMContentLoaded', function() {
+  let queryParameter = location.search.substring(1).split('&')[0].split('=')[1];
+  if (queryParameter !== undefined) {
+    document.getElementById('access-token').value = queryParameter;
+  }
+
+  document.getElementById('send-button').addEventListener('click', function() {
+    let result = document.getElementById('result');
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          result.textContent = xhr.responseText;
+        } else {
+          result.textContent = 'サーバーエラーが発生しました。';
+        }
+      } else {
+        result.textContent = '通信中...';
+      }
+    };
+    xhr.open('POST', 'send_line.php?access-token=' + encodeURIComponent(document.getElementById('access-token').value) + '&message=' + encodeURIComponent(document.getElementById('message').value), true);
+    xhr.send(null);
+  }, false);
+}, false);
