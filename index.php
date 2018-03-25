2c543c0a2f2380957a1711f86f9a90916640a587
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-03-25 23:18:00 +0900

    [update]クエリパラメータからアクセストークンを取得するように変更

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-header('Location: index.html');
+$queryParameter = '';
+if (isset($_GET['access-token'])) {
+  $queryParameter = '?' . $_GET['access-token'];
+}
+header('Location: index.html' . $queryParameter);
 ?>
