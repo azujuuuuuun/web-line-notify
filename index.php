49604e3a3d334482c9a2c66472cf7ab025a16f06
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-03-27 15:57:41 +0900

    [fix]クエリパラメータを修正
    クエリパラメータがうまくリダイレクトされていなかったため

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $queryParameter = '';
 if (isset($_GET['access-token'])) {
-  $queryParameter = '?' . $_GET['access-token'];
+  $queryParameter = '?access-token=' . $_GET['access-token'];
 }
 header('Location: index.html' . $queryParameter);
 ?>
