825380328a87e2cdc0b1a5da846866c723658384
azujuuuuuun <azujuuuuuun@gmail.com>
2018-03-28 00:00:54 +0900

    [update]ボタン押下後、リクエストを表示するように変更

--- js/index.js
+++ js/index.js
@@ -5,11 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   document.getElementById('send-button').addEventListener('click', function() {
+    let accessToken = document.getElementById('access-token').value;
+    let message = document.getElementById('message').value;
+    let request = document.getElementById('request');
+    let response = document.getElementById('response');
     let result = document.getElementById('result');
+
+    request.textContent = 'リクエスト：' + JSON.stringify({ 'access-token': accessToken, 'message': message});
+    response.textContent = '';
+
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
+          response.textContent = 'レスポンス：' + xhr.responseText;
+          result.textContent = '';
           let responseText = JSON.parse(xhr.responseText);
           if (responseText.status === 200) {
             alert('送信しました。');
@@ -22,7 +32,6 @@ document.addEventListener('DOMContentLoaded', function() {
           } else {
             alert('エラー：送信に失敗しました。');
           }
-          result.textContent = xhr.responseText;
         } else {
           result.textContent = 'サーバーエラーが発生しました。';
         }
@@ -30,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
         result.textContent = '通信中...';
       }
     };
-    xhr.open('POST', 'send_line.php?access-token=' + encodeURIComponent(document.getElementById('access-token').value) + '&message=' + encodeURIComponent(document.getElementById('message').value), true);
+    xhr.open('POST', 'send_line.php?access-token=' + accessToken + '&message=' + encodeURIComponent(message), true);
     xhr.send(null);
   }, false);
 }, false);
