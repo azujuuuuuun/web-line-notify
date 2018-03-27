01daa748ff5a1f89f55f9f41cb4d3a9a0410b87d
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2018-03-27 23:23:39 +0900

    [update]メッセージ送信後、アラートを出すように変更

--- js/index.js
+++ js/index.js
@@ -10,6 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
+          let responseText = JSON.parse(xhr.responseText);
+          if (responseText.status === 200) {
+            alert('送信しました。');
+          } else if (responseText.status === 400 && responseText.message === 'message: may not be empty') {
+            alert('エラー：メッセージを入力してください。');
+          } else if (responseText.status === 401 && responseText.message === 'Missing Bearer') {
+            alert('エラー：アクセストークンを入力してください。');
+          } else if (responseText.status === 401 && responseText.message === 'Invalid access token') {
+            alert('エラー：無効なアクセストークンです。');
+          } else {
+            alert('送信に失敗しました。');
+          }
           result.textContent = xhr.responseText;
         } else {
           result.textContent = 'サーバーエラーが発生しました。';
